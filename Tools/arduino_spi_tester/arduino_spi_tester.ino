0799753b1aa271b70262f4c9285e7fb66f42793b
Agustin <a.allegui@gmail.com>
2019-10-17 23:58:52 -0300

    agregado sketch de arduino para probar SPI (todavia no implementado)

---
+++ Tools/arduino_spi_tester/arduino_spi_tester.ino
@@ -0,0 +1,17 @@
+/*  Programa para probar dispositivos con interfaz SPI
+ * 
+ * Toma lo que se le entrega por la consola y lo envia por SPI
+ * 
+ */
+
+// #include <SPI.h>
+
+void setup()
+{
+
+}
+
+void loop()
+{
+
+}
